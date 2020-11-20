73f9d58e25719d4b525de22cb29fbfffc368f73b
Eduardo Ali Garcia Ponce <ali_524-@hotmail.com>
2020-11-20 12:32:56 -0600

    c# version of the python initial commit, about translating an image to braille text

---
+++ InvidentesBraille/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace InvidentesBraille
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.openFile = new System.Windows.Forms.ToolStripLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(868, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(33, 22);
+            this.openFile.Text = "Abrir";
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 502);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(440, 29);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 502);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 543);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel openFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ InvidentesBraille/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace InvidentesBraille
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            char[,] braille2D = new char[,] {
+                
+                //{ " el ", " ⠮ " },
+                //{ " y ", " ⠯ " },
+                //{ " de ", " ⠷ " },
+                //{ " con ", " ⠾ " },
+                //{ " en ", " ⠔ " },
+                //{ " o ", " ⠖ " },
+                //{ " para ", " ⠿ " },
+                { ',', '⠠' },
+                { '/', '⠌' },
+                { '?', '⠹' },
+                { '.', '⠨' },
+                { 'á', '⠁' },
+                { 'é', '⠑' },
+                { 'í', '⠊' },
+                { 'ó', '⠕' },
+                { 'ú', '⠥' },
+                { 'a', '⠁' },
+                { 'b', '⠃' },
+                { 'c', '⠉' },
+                { 'd', '⠙' },
+                { 'e', '⠑' },
+                { 'f', '⠋' },
+                { 'g', '⠛' },
+                { 'h', '⠓' },
+                { 'i', '⠊' },
+                { 'j', '⠚' },
+                { 'k', '⠅' },
+                { 'l', '⠇' },
+                { 'm', '⠍' },
+                { 'ñ', '⠝' },
+                { 'n', '⠝' },
+                { 'o', '⠕' },
+                { 'p', '⠏' },
+                { 'q', '⠟' },
+                { 'r', '⠗' },
+                { 's', '⠎' },
+                { 't', '⠞' },
+                { 'u', '⠥' },
+                { 'v', '⠧' },
+                { 'w', '⠺' },
+                { 'x', '⠭' },
+                { 'y', '⠽' },
+                { 'z', '⠵' },
+                { 'A', '⠁' },
+                { 'B', '⠃' },
+                { 'C', '⠉' },
+                { 'D', '⠙' },
+                { 'E', '⠑' },
+                { 'F', '⠋' },
+                { 'G', '⠛' },
+                { 'H', '⠓' },
+                { 'I', '⠊' },
+                { 'J', '⠚' },
+                { 'K', '⠅' },
+                { 'L', '⠇' },
+                { 'M', '⠍' },
+                { 'N', '⠝' },
+                { 'O', '⠕' },
+                { 'P', '⠏' },
+                { 'Q', '⠟' },
+                { 'R', '⠗' },
+                { 'S', '⠎' },
+                { 'T', '⠞' },
+                { 'U', '⠥' },
+                { 'V', '⠧' },
+                { 'W', '⠺' },
+                { 'X', '⠭' },
+                { 'Y', '⠽' },
+                { 'Z', '⠵' },
+                { '#', '⠼' },
+                { '1', '⠂' },
+                { '2', '⠆' },
+                { '3', '⠲' },
+                { '4', '⠲' },
+                { '5', '⠢' },
+                { '6', '⠖' },
+                { '7', '⠶' },
+                { '8', '⠦' },
+                { '9', '⠔' },
+                { '0', '⠴' },
+                { ' ', ' ' }
+            };
+
+            
+            //var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
+            {
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "Image Files(*.png; *.jpg; *.bmp; *.gif)|*.png; *.jpg; *.bmp; *.gif";
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog1.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog1.OpenFile();
+
+                    //using (StreamReader reader = new StreamReader(fileStream))
+                    //{
+                    //    fileContent = reader.ReadToEnd();
+                    //}
+                }
+            }
+
+            
+            if (filePath != null && filePath != "")
+            {
+                MessageBox.Show("Imagen Cargada Correctamente", "Exito!", MessageBoxButtons.OK);
+                pictureBox1.Image = Image.FromFile(filePath);
+
+                var testImagePath = filePath;
+                try
+                {
+                    using (var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default))
+                    {
+                        using (var img = Pix.LoadFromFile(testImagePath))
+                        {
+                            using (var page = engine.Process(img))
+                            {
+                                var text = page.GetText().TrimEnd();
+                                for(int i = 0; i < 74; i++)
+                                {
+                                    Console.WriteLine("indice de foreach: "+ i);
+                                    Console.WriteLine("valor: " + braille2D[i, 0]);
+                                    Console.WriteLine("remplazo: " + braille2D[i, 1]);
+                                    text = text.Replace(braille2D[i, 0], braille2D[i, 1]);
+                                }
+                                textBox1.AppendText(text);
+                            } 
+                        }
+                    }
+                }
+                catch (Exception err)
+                {
+                    Trace.TraceError(err.ToString());
+                    Console.WriteLine("Unexpected Error: " + err.Message);
+                    Console.WriteLine("Details: ");
+                    Console.WriteLine(err.ToString());
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
