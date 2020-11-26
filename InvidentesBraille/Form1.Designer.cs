9ebc0a1e09be9222292056bd2cca4af3a97d8570
Eduardo Ali Garcia Ponce <ali_524-@hotmail.com>
2020-11-26 15:28:02 -0600

    Installer

--- InvidentesBraille/Form1.Designer.cs
+++ InvidentesBraille/Form1.Designer.cs
@@ -28,36 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.openFile = new System.Windows.Forms.ToolStripLabel();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.toolStrip1.SuspendLayout();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Abrir = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.openFile});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(868, 25);
-            this.toolStrip1.TabIndex = 0;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // openFile
-            // 
-            this.openFile.Name = "openFile";
-            this.openFile.Size = new System.Drawing.Size(33, 22);
-            this.openFile.Text = "Abrir";
-            this.openFile.Click += new System.EventHandler(this.openFile_Click);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // pictureBox1
             // 
@@ -65,9 +62,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox1.Location = new System.Drawing.Point(215, 113);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(404, 502);
+            this.pictureBox1.Size = new System.Drawing.Size(343, 423);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -77,37 +74,189 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(440, 29);
+            this.textBox1.Location = new System.Drawing.Point(578, 113);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(416, 502);
+            this.textBox1.Size = new System.Drawing.Size(333, 423);
             this.textBox1.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.flowLayoutPanel1.Controls.Add(this.Abrir);
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 100);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 448);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(924, 100);
+            this.panel1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(733, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Texto";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label1.Location = new System.Drawing.Point(361, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Imagen";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(739, 16);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(44, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(368, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(59, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Abrir
+            // 
+            this.Abrir.FlatAppearance.BorderSize = 0;
+            this.Abrir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Abrir.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Abrir.ForeColor = System.Drawing.SystemColors.Control;
+            this.Abrir.Image = ((System.Drawing.Image)(resources.GetObject("Abrir.Image")));
+            this.Abrir.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Abrir.Location = new System.Drawing.Point(3, 3);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(197, 46);
+            this.Abrir.TabIndex = 5;
+            this.Abrir.Text = "Abrir";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(2, 6);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(120, 84);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(103, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ImageToBraille";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(3, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enviar a PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(890, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(22, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(868, 543);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(924, 548);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.toolStrip1.ResumeLayout(false);
-            this.toolStrip1.PerformLayout();
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ImageToBraille";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ToolStrip toolStrip1;
-        private System.Windows.Forms.ToolStripLabel openFile;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button Abrir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- InvidentesBraille/Form1.cs
+++ InvidentesBraille/Form1.cs
@@ -1,15 +1,18 @@
-﻿using System;
-using System.ComponentModel;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Diagnostics;
-using System.Drawing;
-using System.Text.RegularExpressions;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 using Tesseract;
+using Image = System.Drawing.Image;
 
 namespace InvidentesBraille
 {
     public partial class Form1 : Form
     {
+        public StringBuilder sb = new StringBuilder();
         public Form1()
         {
             InitializeComponent();
@@ -107,12 +110,13 @@ namespace InvidentesBraille
                 { ' ', ' ' }
             };
 
-            
+
             //var fileContent = string.Empty;
             var filePath = string.Empty;
 
             using (OpenFileDialog openFileDialog1 = new OpenFileDialog())
             {
+                sb.Append("se crea openfiledialog\n");
                 openFileDialog1.InitialDirectory = "c:\\";
                 openFileDialog1.Filter = "Image Files(*.png; *.jpg; *.bmp; *.gif)|*.png; *.jpg; *.bmp; *.gif";
                 openFileDialog1.FilterIndex = 2;
@@ -120,6 +124,7 @@ namespace InvidentesBraille
 
                 if (openFileDialog1.ShowDialog() == DialogResult.OK)
                 {
+                    sb.Append("se abre openfile dialog\n");
                     //Get the path of specified file
                     filePath = openFileDialog1.FileName;
 
@@ -133,47 +138,128 @@ namespace InvidentesBraille
                 }
             }
 
-            
+
             if (filePath != null && filePath != "")
             {
+                //sb.Append("se valida el filepath\n");
                 MessageBox.Show("Imagen Cargada Correctamente", "Exito!", MessageBoxButtons.OK);
                 pictureBox1.Image = Image.FromFile(filePath);
 
                 var testImagePath = filePath;
                 try
                 {
+                    //sb.Append("entra en try de tesseract\n");
                     using (var engine = new TesseractEngine(@"./tessdata", "spa", EngineMode.Default))
                     {
+                        //sb.Append("entra en tesseract engine\n");
                         using (var img = Pix.LoadFromFile(testImagePath))
                         {
+                            //sb.Append("usa pix para loadfromimage\n");
                             using (var page = engine.Process(img))
                             {
+                                //sb.Append("hace engine process para path de imagen\n");
                                 var text = page.GetText().TrimEnd();
-                                for(int i = 0; i < 79; i++)
+                                //sb.Append("justo antes del for para replace\n");
+                                for (int i = 0; i < 79; i++)
                                 {
-                                    Console.WriteLine("indice de foreach: "+ i);
-                                    Console.WriteLine("valor: " + braille2D[i, 0]);
-                                    Console.WriteLine("remplazo: " + braille2D[i, 1]);
+                                    //Console.WriteLine("indice de foreach: " + i);
+                                    //Console.WriteLine("valor: " + braille2D[i, 0]);
+                                    //Console.WriteLine("remplazo: " + braille2D[i, 1]);
                                     text = text.Replace(braille2D[i, 0], braille2D[i, 1]);
                                 }
-                                textBox1.AppendText(text);
-                            } 
+                                textBox1.Text = text;
+                                var txtInput = Path.GetTempPath() + "log.txt";
+                                File.WriteAllText(txtInput, text);
+                                //sb.Append("se termina de escribir el output en un path temporal\n");
+                                //File.AppendAllText("logger.txt", //sb.ToString());
+                                //sb.Clear();
+                            }
                         }
                     }
                 }
                 catch (Exception err)
                 {
+                    //sb.Append("Se encontro algun error durante el try tesseract\n"+ err.ToString()+"\n");
                     Trace.TraceError(err.ToString());
                     Console.WriteLine("Unexpected Error: " + err.Message);
                     Console.WriteLine("Details: ");
                     Console.WriteLine(err.ToString());
+                    //File.AppendAllText("logger.txt", //sb.ToString());
+                    //sb.Clear();
                 }
             }
         }
 
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
 
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "PDF Files|*.pdf";
+            dlg.FilterIndex = 0;
+
+            string fileName = string.Empty;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                var txtOutput = dlg.FileName;
+
+                //sb.Append("Entra al envio de PDF\n");
+                //  public static final String FONT = "resources/fonts/Cardo-Regular.ttf";
+                //  public static final String TEXT = "The Cardo family of fonts supports this character: \u2609";
+                //
+                //  public void createPdf(String dest) throws IOException, DocumentException {
+                //  Document document = new Document();
+                //  PdfWriter.getInstance(document, new FileOutputStream(dest));
+                //  document.open();
+                //  BaseFont bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                //  Font f = new Font(bf, 12);
+                //  Paragraph p = new Paragraph(TEXT, f);
+                //  document.add(p);
+                //  document.close();
+                //}
+
+                BaseFont bf = BaseFont.CreateFont("font/EversonMono.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+                Font f = new Font(bf, 12);
+
+                var txtInput = Path.GetTempPath() + "log.txt";
+                //Read the Data from Input File
+                StreamReader rdr = new StreamReader(txtInput);
+                //sb.Append("inicia streamreader\n");
+                //Create a New instance on Document Class
+                Document doc = new Document();
+                //Create a New instance of PDFWriter Class for Output File
+                try
+                {
+                    //sb.Append("hace try de filestream\n");
+                    PdfWriter.GetInstance(doc, new FileStream(txtOutput, FileMode.Create));
+                }
+                catch (Exception err)
+                {
+                    //sb.Append("hubo error de filestream\n");
+                }
+                //Open the Document
+                doc.Open();
+                //sb.Append("abre instancia de doc\n");
+                //Add the content of Text File to PDF File
+                doc.Add(new Paragraph(rdr.ReadToEnd(), f));
+                //sb.Append("agrega font a doc\n");
+                //Close the Document
+                doc.Close();
+                //sb.Append("cierra instancia de doc\n");
+                //Open the Converted PDF File
+                Process.Start(txtOutput);
+                rdr.Close();
+                //sb.Append("abre pdf\n");
+                //File.Delete(txtInput);
+                //sb.Append("borra txt\n");
+                //File.AppendAllText("logger.txt", //sb.ToString());
+                //sb.Clear();
+            }
         }
     }
 }
